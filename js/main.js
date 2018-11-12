674296261b7840b8ac13e2700ad7fdbb3090d2da
Liberty MABASO <lmabaso@e4r11p1.wethinkcode.co.za>
2018-11-12 17:34:17 +0200

    auto commit

--- js/main.js
+++ js/main.js
@@ -1,38 +1,27 @@
-var video = document.getElementById('video');
-var canvas = document.getElementById('canvas');
-var ctx = canvas.getContext('2d');
-var photo = document.getElementById('photo');
-var tmp;
-var input = document.querySelector('input[type="file"]');
+var video = document.querySelector('#camera-stream'),
+    image = document.querySelector('#snap'),
+    start_camera = document.querySelector('#start-camera'),
+    controls = document.querySelector('.controls'),
+    take_photo_btn = document.querySelector('#take-photo'),
+    delete_photo_btn = document.querySelector('#delete-photo'),
+    download_photo_btn = document.querySelector('#save-photo'),
+    error_message = document.querySelector('#error-message'),
+    select_pic = document.querySelector('#upload-file-local'),
+    upload_pic = document.querySelector('#upload-pic');
 
-function gender(str) {
-    var mysql = require('mysql');
+navigator.getMedia =    navigator.getUserMedia ||
+                        navigator.webkitGetUserMedia ||
+                        navigator.mozGetUserMedia ||
+                        navigator.mediaDevices.getUserMedia||
+                        navigator.msGetUserMedia;
 
-    var con = mysql.createConnection({
-        host: "localhost",
-        user: "root",
-        password: "zxcvb12345",
-        database: "matcha"
-    });
 
-    con.connect(function(err) {
-    if (err) throw err;
-        var sql = "UPDATE profiles SET user_id = '1' WHERE user_gender = 'male'";
-        con.query(sql, function (err, result) {
-            if (err) throw err;
-            console.log(result.affectedRows + " record(s) updated");
-        });
-    });
-
-}
-
-(function()
+if (!navigator.getMedia)
 {
-    navigator.getMedia =    navigator.getUserMedia ||
-                            navigator.webkitGetUserMedia ||
-                            navigator.mozGetUserMedia ||
-                            navigator.mediaDevices.getUserMedia||
-                            navigator.msGetUserMedia;
+  displayErrorMessage("Your browser doesn't have support for the navigator.getUserMedia interface.");
+}
+else
+{
     navigator.getMedia({
         video: true,
         audio: false
@@ -40,86 +29,179 @@ function gender(str) {
         video.srcObject = stream;
         video.play();
     }, function (error) {
-
+        displayErrorMessage("There was an error with accessing the camera stream: " + error.name, error);
     });
+}
 
-    input.addEventListener('change', function(e){
-        const reader = new FileReader();
-    
-        var img1 = new Image();
-        img1.onload = function() {
-            ctx.drawImage(img1, 0, 0, 400, 300);
-        }
-        reader.onload = function() {
-            img1.src = reader.result;
-        }
-        reader.readAsDataURL(input.files[0]);
-    }, false);
-}) ();
-
-
-
-
-function capture(data)
+start_camera.addEventListener("click", function(e)
 {
-    if (data === "capture")
-    {
-        ctx.drawImage(video, 0, 0, 400, 300);
-        document.getElementById('canvas').style = "display:block";
-        document.getElementById('video').style = "display:none";
-        document.getElementById("capture").style = "display:none";
-        document.getElementById('upload').style = "display:block";
-        document.getElementById('new').style = "display:block";
-        document.getElementById('upload').disabled = true;
-    }
-    else if (data === "new")
-    {
-        document.getElementById('canvas').style = "display:none";
-        document.getElementById('video').style = "display:block";
-        document.getElementById("capture").style = "display:block";
-        document.getElementById("new").style = "display:none";
-        document.getElementById("upload").style = "display:none";
-        document.getElementById('photoform').disabled = true;
-    }
-    else if (data === "save")
-    {
-        photo.setAttribute('value', canvas.toDataURL('Image/png'));
-        document.getElementById('photoform').disabled = false;
-    }
-    else if (data === "camera")
-    {
-        document.getElementById('camera').style = "display:block";
-        document.getElementById('cam').style = "display:none";
-        document.getElementById('upl').style = "display:none";
-    }
-    else if (data === "upload")
-    {
-        document.getElementById('camera').style = "display:block";
-        document.getElementById('canvas').style = "display:block";
-        document.getElementById('video').style = "display:none";
-        document.getElementById("capture").style = "display:none";
-        document.getElementById('upload').style = "display:block";
-        document.getElementById('new').style = "display:none";
-        document.getElementById('upload').disabled = true;
-        document.getElementById('upl').style = "display:none";
-        document.getElementById('cam').style = "display:none";
-    }
+    start_camera.style = "display: none";
+    upload_pic.style = "display: none";
+    e.preventDefault();
+    video.play();
+    showVideo();
+});
 
-}
 
-function superimpose() {
-    var img = document.getElementById('filter').value;
-    if (img != "")
-    {
-        document.getElementById('upload').disabled = false;
-    }
-    document.getElementById('inner-box').setAttribute('style', 'background-image: url('+img+')')
-    document.getElementById('super_img').setAttribute('value', img);
-    // ctx.drawImage(document.getElementById('uploadpic').value, 0, 0, 400, 300);
-    // document.getElementById('inner-box').style.backgroundImage = url(img);
-}
-
-function choosepic() 
+upload_pic.addEventListener("click", function(e)
 {
-    alert("lol");
+    start_camera.style = "display: none";
+    upload_pic.style = "display: none";
+    e.preventDefault();
+    select_pic.style = "display: block";
+});
+
+take_photo_btn.addEventListener("click", function(e)
+{
+  e.preventDefault();
+  var snap = takeSnapshot(); 
+  image.setAttribute('src', snap);
+  image.classList.add("visible");
+  delete_photo_btn.classList.remove("disabled");
+  download_photo_btn.classList.remove("disabled");
+
+//   download_photo_btn.href = snap;
+  video.pause();
+});
+
+
+delete_photo_btn.addEventListener("click", function(e)
+{
+    e.preventDefault();
+    image.setAttribute('src', "");
+    image.classList.remove("visible");
+    delete_photo_btn.classList.add("disabled");
+    download_photo_btn.classList.add("disabled");
+    video.play();
+});
+
+function showVideo()
+{
+    hideUI();
+    video.classList.add("visible");
+    controls.classList.add("visible");
 }
+
+function takeSnapshot()
+{
+    var hidden_canvas = document.querySelector('canvas'),
+        context = hidden_canvas.getContext('2d');
+    var width = video.videoWidth,
+        height = video.videoHeight;
+        
+    if (width && height)
+    {
+        hidden_canvas.width = width;
+        hidden_canvas.height = height;
+        context.drawImage(video, 0, 0, width, height);
+        return hidden_canvas.toDataURL('image/png');
+    }
+}
+
+
+function displayErrorMessage(error_msg, error)
+{
+    error = error || "";
+    if(error)
+    {
+        console.log(error);
+    }
+    error_message.innerText = error_msg;
+    hideUI();
+    error_message.classList.add("visible");
+}
+
+
+function hideUI(){
+    controls.classList.remove("visible");
+    start_camera.classList.remove("visible");
+    video.classList.remove("visible");
+    snap.classList.remove("visible");
+    error_message.classList.remove("visible");
+}
+
+// var video = document.getElementById('video');
+var canvas = document.querySelector('canvas');
+var ctx = canvas.getContext('2d');
+// var photo = document.getElementById('photo');
+// var tmp;
+var input = document.querySelector('input[type="file"]');
+
+// (function()
+// {
+//     navigator.getMedia =    navigator.getUserMedia ||
+//                             navigator.webkitGetUserMedia ||
+//                             navigator.mozGetUserMedia ||
+//                             navigator.mediaDevices.getUserMedia||
+//                             navigator.msGetUserMedia;
+//     navigator.getMedia({
+//         video: true,
+//         audio: false
+//     }, function(stream) {
+//         video.srcObject = stream;
+//         video.play();
+//     }, function (error) {
+
+//     });
+
+input.addEventListener('change', function(e){
+    const reader = new FileReader();
+    canvas.style = "display:block";
+    var img1 = new Image();
+    var width = video.videoWidth,
+        height = video.videoHeight;
+    img1.onload = function() {
+        canvas.width = width;
+        canvas.height = height;
+        ctx.drawImage(img1, 0, 0, width, height);
+    }
+    reader.onload = function() {
+        img1.src = reader.result;
+    }
+    reader.readAsDataURL(input.files[0]);
+}, false);
+// }) ();
+
+// function capture(data)
+// {
+//     if (data === "capture")
+//     {
+//         ctx.drawImage(video, 0, 0, 400, 300);
+//         document.getElementById('canvas').style = "display:block";
+//         document.getElementById('video').style = "display:none";
+//         document.getElementById("capture").style = "display:none";
+//         document.getElementById('new').style = "display:block";
+//         photo.setAttribute('value', canvas.toDataURL('Image/png'));
+//         document.getElementById('photoform').disabled = false;
+//     }
+//     else if (data === "new")
+//     {
+//         document.getElementById('canvas').style = "display:none";
+//         document.getElementById('video').style = "display:block";
+//         document.getElementById("capture").style = "display:block";
+//         document.getElementById("new").style = "display:none";
+//         document.getElementById("upload").style = "display:none";
+//         document.getElementById('photoform').disabled = true;
+//     }
+//     else if (data === "camera")
+//     {
+//         document.getElementById('camera').style = "display:block";
+//         document.getElementById('cam').style = "display:none";
+//         document.getElementById('upl').style = "display:none";
+//         document.getElementById('photoform').disabled = false;
+//         photo.setAttribute('value', canvas.toDataURL('Image/png'));
+//     }
+//     else if (data === "upload")
+//     {
+//         document.getElementById('camera').style = "display:block";
+//         document.getElementById('canvas').style = "display:block";
+//         document.getElementById('video').style = "display:none";
+//         document.getElementById("capture").style = "display:none";
+//         document.getElementById('new').style = "display:none";
+//         photo.setAttribute('value', canvas.toDataURL('Image/png'));
+//         document.getElementById('photoform').disabled = false;
+//         document.getElementById('upl').style = "display:none";
+//         document.getElementById('cam').style = "display:none";
+//     }
+
+// }
