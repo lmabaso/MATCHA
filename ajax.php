784369c6f650602c409dbd288d15c6b860d492c0
Liberty MABASO <lmabaso@e4r11p1.wethinkcode.co.za>
2018-11-12 17:33:40 +0200

    auto commit

--- ajax.php
+++ ajax.php
@@ -2,9 +2,14 @@
 include_once 'header.php';
 
 // if (Input::exists()) {
+var_dump(Input::get('long'));
+echo '<script>alert("gets here: ")</script>';
 $data = DB::getInstance();
-
-$data->update('profiles', $user->data()->user_id, array('user_gender' => Input::get('gender')));
-echo "success";
+if (Input::get('position'))
+{
+    
+    $data->update('users', $user->data()->user_id, array('user_pos_lon' => Input::get('long'), 'user_pos_lat' => Input::get('lat')));
+    echo "success";
+}
 // }
 ?>
