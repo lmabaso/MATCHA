9aaf22aad3c1a907beb3a8a4b23da1dbb829b62d
Liberty MABASO <lmabaso@e4r11p1.wethinkcode.co.za>
2018-11-12 17:34:10 +0200

    auto commit

--- js/gps.js
+++ js/gps.js
@@ -1,13 +1,19 @@
-var x = document.getElementById("demo");
-
 function getLocation() {
     if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(showPosition);
+        navigator.geolocation.getCurrentPosition(loadPosition);
     } else {
         x.innerHTML = "Geolocation is not supported by this browser.";
     }
 }
-function showPosition(position) {
-    x.innerHTML = "Latitude: " + position.coords.latitude + 
-    "<br>Longitude: " + position.coords.longitude; 
+function loadPosition(position) {
+    $.ajax({
+        url: "ajax.php",
+        type: "POST",
+        data:  {position: 'true', lat: position.coords.latitude, long: position.coords.longitude},
+        success: function (response) {        
+            console.log("suceess")
+        }
+    });
 }
+
+getLocation();
