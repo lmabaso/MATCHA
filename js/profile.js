90ae7d68cf8d9137bb77336ebdb8a65ab8a5056d
Liberty MABASO <lmabaso@e4r11p1.wethinkcode.co.za>
2018-11-16 17:21:14 +0200

    auto commit

---
+++ js/profile.js
@@ -0,0 +1,15 @@
+var add_interest_btn = document.querySelector('#add_interest');
+
+add_interest_btn.addEventListener('click', function(e)
+{
+    e.preventDefault();
+    $.ajax({
+        url: "ajax.php",
+        type: "POST",
+        data:  {add_interest: 'true', interest: document.getElementById('interests').value},
+        success: function (response) {        
+            alert("suceessfully added")
+            location.reload();
+        }
+    });
+});
