8c4670245f2a55dd0418feed07484cb2e1b5bd20
Liberty MABASO <lmabaso@e4r11p1.wethinkcode.co.za>
2018-12-18 18:51:19 +0200

    auto commit

---
+++ fetch_user.php
@@ -0,0 +1,33 @@
+<?php
+
+include_once 'header.php';
+
+$data = DB::getInstance();
+$result2 = new Photo();
+
+$result = $data->query("SELECT * FROM users ")->results();
+echo ' <ul>';
+foreach($result as $row)
+{
+
+    $status = '';
+    $current_timpstamp = strtotime(date('Y-m-d H:i:s') . '-10 seconds');
+    $current_timpstamp = date('Y-m-d H:i:s', $current_timpstamp);
+    $user_last_active = $row->last_activity;
+echo '
+<li class="contact">
+    <div style="display: flex">';
+$pics = $result2->find($row->user_id)->results();
+echo '<img src="data:image/jpeg;base64,' . base64_encode($pics[0]->pic_dir) . '" style=" border-radius: 50%; margin: 0px 10px 0px 10px; width: 50px; height: 50px; object-fit:cover;" />
+        <div styel= "line-height: none">
+            <strong>' . $row->user_username . '</strong> -:-';
+            echo ($user_last_active > $current_timpstamp) ? '<font color="green">Online</font><br>' : '<font color="gray">Offline</font><br>';
+            echo'You just got LITT up, Mike.
+        </div>
+    </div>
+    <hr>
+</li>
+';
+}
+echo '</ul>';
+?>
